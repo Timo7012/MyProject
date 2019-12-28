cfbdd74caaed6cc34970cc217e4598a9289a4a3c
Timur <mahmudov.timur701@gmail.com>
2019-12-29 00:30:48 +0300

    upd13

--- admin_page.php
+++ admin_page.php
@@ -1,11 +1,10 @@
 <?php
 require "connect_db.php";
-session_start();
 if(isset($_SESSION['login'])){
 	$exs_p = "SELECT `priv` FROM `auth1` WHERE `login`='".$_SESSION['login']."';";
 	$exs_pq = mysqli_query($link,$exs_p);
 	$e=$exs_pq->fetch_assoc();
-	if (!($e['priv']==1 or $e['priv']==2)){
+	if ($e['priv']!=2){
 		header("Location: main_page.php");
 	}
 }
@@ -16,26 +15,27 @@ if(isset($_SESSION['login'])){
 <link rel="stylesheet" href="newsite.css">
 </head>
 <body class="bodybackground">
-<div class="auth_1">
+<div class="auth_11">
 	<h1>Admins page</h1>
 	<form action="chp_server.php" method="POST">
 	Login
 	<select name="log">
 	 <?php
-	 for($i=1;$i<256;$i++){
-	 $exs_a="SELECT `login` FROM `auth1` WHERE `id`='$i';";
-	 $exs_aq=mysqli_query($link,$exs_a);
-	 $ea = $exs_aq->fetch_assoc();
-      echo '"<option>'.$ea['login'].'</option>"';};
+	 $ea="SELECT `login` FROM `auth1`;";
+	 $ea=mysqli_query($link,$ea);
+	 for($i=1;$ea_q=$ea->fetch_assoc();$i++){
+      echo '"<option>'.$ea_q['login'].'</option>"';};
      ?>
  	</select>
-	<p>Priv</p>
+ 	<br>
+	<p>Priveleges
 	<select name="i">
 	 <?php
 	 for($i=0;$i<3;$i++){ 
       echo '"<option name="i">'.$i.'</option>"';};
      ?>
     </select>
+    </p><br>
 	<input id="login" type="submit" value="Change">
 </form>
 </div>
